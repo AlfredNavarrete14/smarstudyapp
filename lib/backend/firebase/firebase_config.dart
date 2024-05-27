a5c22a8ad3de841874e4263e6bdab7ee1c0c10c8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-27 19:46:43 +0000

    Updating to latest FlutterFlow output.

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,13 +5,13 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-            apiKey: "AIzaSyCR0vp8xHy3wRCXWy8Wj306X7jrXljDBJ4",
-            authDomain: "smartstudy-8adf8.firebaseapp.com",
-            projectId: "smartstudy-8adf8",
-            storageBucket: "smartstudy-8adf8.appspot.com",
-            messagingSenderId: "1057939842562",
-            appId: "1:1057939842562:web:2176564ccebdcc03dee91b",
-            measurementId: "G-DMYGZLBQN6"));
+            apiKey: "AIzaSyCxyweECfbEtTN8UYA4OJarZv1anbShb7o",
+            authDomain: "smartstudy-5f020.firebaseapp.com",
+            projectId: "smartstudy-5f020",
+            storageBucket: "smartstudy-5f020.appspot.com",
+            messagingSenderId: "35642686587",
+            appId: "1:35642686587:web:94a33e2cdb46fcd5f9f6de",
+            measurementId: "G-14ZNW6VSXJ"));
   } else {
     await Firebase.initializeApp();
   }
